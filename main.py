f8fb2712b6d89e7b4b68e620114bab316d419c0c
Derion Williams <derionwllms@gmail.com>
2024-08-05 02:49:28 -0500

    counted words

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        print(count_words(file_contents))
+
+def count_words(string):
+    return len(string.split())
 
 main()
